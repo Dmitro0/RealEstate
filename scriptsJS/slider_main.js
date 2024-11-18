b71dac3f2535d196e62fa18bc2f2f9fbf39e799a
Dmitriy <@>
2024-11-18 19:28:51 +0300

    renaming

--- scriptsJS/slider_main.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-const maxSlide = slides.length - 3;
-
-function moveSlide(direction) {
-    currentSlide = currentSlide == maxSlide ? 0 : Math.max(0, Math.min(currentSlide + direction, maxSlide));
-    slider.style.transform = `translateX(-${currentSlide * 33.333}%)`;
-}
